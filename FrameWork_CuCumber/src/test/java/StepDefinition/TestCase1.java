38901389c11a73b761ce46ec5f6c8d7f3ab44dc9
admin <vinoth13.marolix@gmail.com>
2024-01-31 16:39:58 +0530

    Seventh Commit

--- FrameWork_CuCumber/src/test/java/StepDefinition/TestCase1.java
+++ FrameWork_CuCumber/src/test/java/StepDefinition/TestCase1.java
@@ -1,5 +1,15 @@
 package StepDefinition;
 
 public class TestCase1 {
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void sys() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
